c55905d57282f0cdd662864ebffceb7dfea4e0f0
i-AlvaroE <aemaem92@gmail.com>
2023-10-08 19:35:34 +0200

    jsfirst

---
+++ S1/1.variables/exercise-7.js
@@ -0,0 +1,5 @@
+let globalBasePrice = 25000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice:75000 };
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 95000};
+ 
+console.log(car1 , car2) ;
